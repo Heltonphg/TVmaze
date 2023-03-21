f8f06a27dc35f39f88dc025032ec6a6f78eea44b
heltonbase <111578214+heltonbase@users.noreply.github.com>
2023-03-21 08:56:22 -0300

    test: test starRating component

--- src/components/StarRating/__tests__/StarRating.spec.tsx
+++ src/components/StarRating/__tests__/StarRating.spec.tsx
@@ -3,13 +3,24 @@ import {StarRating} from '../StarRating';
 import {render} from '@testing-library/react-native';
 
 describe('StarRating', () => {
-  it('should render correctly', () => {
-    render(
-      <StarRating
-        rating={{
-          average: 4.5,
-        }}
-      />,
-    );
+  describe('rating is passed', () => {
+    it('show the average', () => {
+      const {getByText} = render(<StarRating rating={{average: 4.5}} />);
+      expect(getByText('4.5')).toBeTruthy();
+    });
+    it('show the star icon', () => {
+      const {getByTestId} = render(<StarRating rating={{average: 4.5}} />);
+      expect(getByTestId('starIcon')).toBeTruthy();
+    });
+  });
+  describe('rating is not passed', () => {
+    it('does not show the average', () => {
+      const {queryByText} = render(<StarRating />);
+      expect(queryByText('4.5')).toBeNull();
+    });
+    it('does not show the star icon', () => {
+      const {queryByTestId} = render(<StarRating />);
+      expect(queryByTestId('starIcon')).toBeNull();
+    });
   });
 });
